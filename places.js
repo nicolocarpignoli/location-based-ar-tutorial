1d0b3047a38d1a30d5494a294caee84bbd5c8aa7
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2019-10-18 17:09:52 +0200

    size

--- places.js
+++ places.js
@@ -88,9 +88,9 @@ function renderPlaces(places) {
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         model.setAttribute('gltf-model', '#animated-asset');
-        // model.setAttribute('rotation', '0 180 0');
+        model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '0.05 0.05 0.05');
+        model.setAttribute('scale', '0.5 0.5 0.5');
 
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
